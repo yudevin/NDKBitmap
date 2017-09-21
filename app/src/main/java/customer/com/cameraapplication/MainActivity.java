ec2faeeef833b64f09c019250ed31d0fd4b3ff7c
yuxumou <yuxumou@nme.cn>
2017-09-21 11:49:17 +0800

    删除无用代码

--- app/src/main/java/customer/com/cameraapplication/MainActivity.java
+++ app/src/main/java/customer/com/cameraapplication/MainActivity.java
@@ -5,8 +5,7 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -18,6 +17,7 @@ import customer.com.cameraapplication.meitu.NDKImageUtil;
 public class MainActivity extends Activity {
 
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,19 +26,6 @@ public class MainActivity extends Activity {
     }
 
 
-
-    Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            Bitmap bitmap = (Bitmap) msg.getData().get("bitmap");
-            imageView.setImageBitmap(bitmap);
-        }
-    };
-
-
-
-    ImageView imageView;
 
     @Override
     protected void onResume() {
@@ -94,36 +81,6 @@ public class MainActivity extends Activity {
         findViewById(R.id.ndk_hahajing_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-
-                //ndk获取
-//                int[] ndkImage = NDKImageUtil.getNDKBitmap(buffer, w, h);
-//
-//                new Thread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        long time = System.currentTimeMillis();
-//
-//
-//                        final int w = bitmap.getWidth();
-//                        final int h = bitmap.getHeight();
-//
-//                        final int[] buffer = new int[w * h];
-//                        bitmap.getPixels(buffer, 0, w, 1, 1, w - 1, h - 1);
-//                        int[] ndkImage = NDKImageUtil.toHahajing(buffer, w, h,w/2,h/2,h/9,0.3f);
-//                        Bitmap rebitmap = Bitmap.createBitmap(ndkImage,w, h, Bitmap.Config.ARGB_8888);
-//                        Message msg = Message.obtain();
-//                        Bundle bundle = new Bundle();
-//                        bundle.putParcelable("bitmap",rebitmap);
-//                        msg.setData(bundle);
-//                        handler.sendMessage(msg);
-//                        time = (System.currentTimeMillis() - time);
-//                        Toast.makeText(MainActivity.this, "NDK 处理 hha bitmap 时间 " + time, Toast.LENGTH_LONG).show();
-//                    }
-//                }).start();
-
-
 
                 long time = System.currentTimeMillis();
 
